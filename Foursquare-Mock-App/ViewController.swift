3b282c299e138d7b91852135cf3c19892bfa013d
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-21 15:24:24 -0500

    Initial Commit

---
+++ Foursquare-Mock-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foursquare-Mock-App
+//
+//  Created by Kelby Mittan on 2/21/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
