be6df904e6453ec41df930ee94669601c2e1f68e
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-21 16:34:44 -0500

    Pushing project, testing .gitignore and sync

---
+++ Foursquare-Mock-App/APIKeys/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Foursquare-Mock-App
+//
+//  Created by Gregory Keeley on 2/21/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let key = ""
+}
