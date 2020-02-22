1a02b87f68bbf1fe21d31b73276e3e6ccc31237d
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-22 16:38:10 -0500

    performed unit test for json and basic model

--- Foursquare-Mock-AppTests/Foursquare_Mock_AppTests.swift
+++ Foursquare-Mock-AppTests/Foursquare_Mock_AppTests.swift
@@ -10,25 +10,96 @@ import XCTest
 @testable import Foursquare_Mock_App
 
 class Foursquare_Mock_AppTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testJSONStarbucks() {
+        
+        
+        let jsonData = """
+        {
+        "meta": {
+            "code": 200,
+            "requestId": "5e5191c8df2774001bf56295"
+        },
+        "response": {
+            "venues": [
+                {
+                    "id": "5c13bf838afbe0002de55061",
+                    "name": "Starbucks Reserve Roastery",
+                    "location": {
+                        "address": "61 9th Ave",
+                        "crossStreet": "at W 15th St",
+                        "lat": 40.74166158826782,
+                        "lng": -74.00507982765883,
+                        "labeledLatLngs": [
+                            {
+                                "label": "display",
+                                "lat": 40.74166158826782,
+                                "lng": -74.00507982765883
+                            }
+                        ],
+                        "postalCode": "10011",
+                        "cc": "US",
+                        "city": "New York",
+                        "state": "NY",
+                        "country": "United States",
+                        "formattedAddress": [
+                            "61 9th Ave (at W 15th St)",
+                            "New York, NY 10011",
+                            "United States"
+                        ]
+                    },
+                    "categories": [
+                        {
+                            "id": "4bf58dd8d48988d1e0931735",
+                            "name": "Coffee Shop",
+                            "pluralName": "Coffee Shops",
+                            "shortName": "Coffee Shop",
+                            "icon": {
+                                "prefix": "https://ss3.4sqi.net/img/categories_v2/food/coffeeshop_",
+                                "suffix": ".png"
+                            },
+                            "primary": true
+                        }
+                    ],
+                    "referralId": "v-1582403930",
+                    "hasPerk": false
+            }]
+            }
         }
+        """.data(using: .utf8)!
+        
+        struct FoursquareSearch: Codable {
+            let response: Venues
+        }
+        
+        struct Venues: Codable {
+            let venues: [Venue]
+        }
+        
+        struct Venue: Codable {
+            let id: String
+            let name: String
+            let location: LocationInfo
+        }
+        
+        struct LocationInfo: Codable {
+            let address: String
+            let crossStreet: String
+            let lat: Double
+            let lng: Double
+        }
+        
+        let expectedLat = 40.74166158826782
+        
+        do {
+            let foursquare = try JSONDecoder().decode(FoursquareSearch.self, from: jsonData)
+            let supLat = foursquare.response.venues.first?.location.lat ?? 0.0
+            XCTAssertEqual(expectedLat, supLat)
+        } catch {
+            XCTFail("decoding error: \(error)")
+        }
+        
     }
-
+    
+    
 }
